f6c9765f0056cf25ea189077ddc815b25f40b78e
SimonRunnquist <Simon.Runnquist@europe.com>
2015-10-09 16:53:46 +0200

    Fixat grejjer va

--- FeedMeNomNom/DAO/getFeed.cs
+++ FeedMeNomNom/DAO/getFeed.cs
@@ -8,21 +8,32 @@ using System.Xml.Linq;
 using System.ServiceModel.Syndication;
 using System.Net;
 using System.IO;
+using FeedMeNomNom.VO;
 
 namespace FeedMeNomNom.DAO
 {
     class getFeed
     {
+        UserVO listObjectVO = new UserVO();
         private string name;
+        string[] feedArray = new string[200];
+        string[] unfilteredURL = new string[400];
+        string[] filteredURL = new string[200];
         
-        public void googleGet(string userURL) {
+        public string[] getPod(string userURL) {
             //string url = "http://news.google.fr/nwshp?hl=fr&tab=wn&output=rss";
             //http://alexosigge.libsyn.com//rss
             //http://hannahoamanda.libsyn.com/rss
 
+
+            
+
             string getURL = userURL;
-            using (XmlReader reader = XmlReader.Create(getURL))
+            using (XmlReader reader = XmlReader.Create(getURL)) //If null måste fixas, blir fel vid tomt eller felaktigt fält
             {
+                var i = 0;
+                    
+
                 SyndicationFeed feed = SyndicationFeed.Load(reader);
                 //Console.WriteLine(feed.Title.Text);
                 //Console.WriteLine(feed.Links[0].Uri);
@@ -30,13 +41,42 @@ namespace FeedMeNomNom.DAO
                 
                 foreach (SyndicationItem item in feed.Items)
                 {
-                    Console.WriteLine(item.Title.Text);
-                    foreach (var link in item.Links) {
+                        
+                        feedArray[i] = item.Title.Text;
+
+                         foreach (var link in item.Links)
+                            {
+                                unfilteredURL[i] = link.Uri.ToString();
+                            }
+                        i++;
+
+                   /* foreach (var link in item.Links) {
                         Console.WriteLine(link.Uri);
-                    }
+                    }*/
                 }
 
+                return feedArray;
+
             }
+        }
+
+        public string[] getDownloadURL() {
+
+            for (var i = 0; i < unfilteredURL.Length; i++) {
+                string getArrayURL = unfilteredURL[i];
+                Console.WriteLine(getArrayURL);
+
+                if (getArrayURL == null) {
+                    break;
+                }
+
+                else if (getArrayURL.Contains("mp3"))
+                {
+                    filteredURL[i] = getArrayURL;
+                }
+            }
+                return filteredURL;
+        
         }
 
         public void downloadMP3() {
@@ -62,5 +102,26 @@ namespace FeedMeNomNom.DAO
             }
         }
 
+        public void wipeCollectedData() {
+            wipeFilteredData();
+            wipeFeedData();
+            wipeUnfilteredData();
+        }
+
+        public void wipeFeedData() {
+            for (var i = 0; i < 200; i++)
+                feedArray[i] = null;
+        }
+
+        public void wipeUnfilteredData()
+        {
+            for (var i = 0; i < 400; i++)
+                unfilteredURL[i] = null;
+        }
+        public void wipeFilteredData()
+        {
+            for (var i = 0; i < 200; i++)
+                filteredURL[i] = null;
+        }
     }
 }
--- FeedMeNomNom/MainWindow.xaml.cs
+++ FeedMeNomNom/MainWindow.xaml.cs
@@ -27,7 +27,10 @@ namespace FeedMeNomNom
     {
         
         getFeed test = new getFeed();
+        addItem getItem = new addItem();
         saveXML createXml = new saveXML();
+        string[] podcast;
+        string[] downloadURL;
 
         public MainWindow()
         {
@@ -35,11 +38,7 @@ namespace FeedMeNomNom
             createXml.createBaseXml();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
-            test.googleGet(tbURL.Text);
-        }
+      
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
@@ -48,8 +47,37 @@ namespace FeedMeNomNom
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
-            tbURL.Text = test.Name;
+            
             test.Name = "Mac!";
+            tbURL.Text = test.Name;
+        }
+
+        private void getFeed_button_Click(object sender, RoutedEventArgs e)
+        {
+            listBox_Feed.Items.Clear();
+            podcast = test.getPod(tbURL.Text);
+            downloadURL = test.getDownloadURL();
+
+            //for (var i = 0; i < 20; i++) {
+              //  listBox_Feed.Items.Add(test.getPod(tb);
+            //}
+
+
+            for (var i = 0; i < podcast.Length; i++)
+            {
+                if (podcast[i] == null)
+                {
+                    break;
+                }
+                else
+                {
+                    listBox_Feed.Items.Add(podcast[i]);
+                    listBox_Feed.Items.Add(downloadURL[i]);
+                    listBox_Feed.Items.Add(" ");
+                }
+            }
+
+            test.wipeCollectedData();
         }
 
         
